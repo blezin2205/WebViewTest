996a749c703d217d0219735d0e5c07e24b868fb8
Blezin <blezin2205@gmail.com>
2020-12-23 21:31:03 +0200

    Initial Commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Blezin on 23.12.2020.
+//  Copyright © 2020 Blezin'sDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
